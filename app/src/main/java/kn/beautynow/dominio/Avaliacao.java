61cf02fa4a43977a07ff2d36d22770cbb987b7a8
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2018-12-02 19:32:59 -0300

    up data

---
+++ app/src/main/java/kn/beautynow/dominio/Avaliacao.java
@@ -0,0 +1,49 @@
+package kn.beautynow.dominio;
+
+public class Avaliacao {
+    private String id;
+    private int preco;
+    private int qds;
+    private int qda;
+    private int pontualidade;
+
+    private int getPreco() {
+        return preco;
+    }
+
+    private void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    private int getQds() {
+        return qds;
+    }
+
+    private void setQds(int qds) {
+        this.qds = qds;
+    }
+
+    private int getQda() {
+        return qda;
+    }
+
+    private void setQda(int qda) {
+        this.qda = qda;
+    }
+
+    private int getPontualidade() {
+        return pontualidade;
+    }
+
+    private void setPontualidade(int pontualidade) {
+        this.pontualidade = pontualidade;
+    }
+
+    private String getId() {
+        return id;
+    }
+
+    private void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/kn/beautynow/dominio/Promocao.java
@@ -0,0 +1,49 @@
+package kn.beautynow.dominio;
+
+public class Promocao {
+    private String id;
+    private String nome;
+    private int valor;
+    private Servico servico;
+    private Fornecedor fornecedor;
+
+    private String getId() {
+        return id;
+    }
+
+    private void setId(String id) {
+        this.id = id;
+    }
+
+    private String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private int getValor() {
+        return valor;
+    }
+
+    private void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    private Servico getServico() {
+        return servico;
+    }
+
+    private void setServico(Servico servico) {
+        this.servico = servico;
+    }
+
+    private Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    private void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+}
