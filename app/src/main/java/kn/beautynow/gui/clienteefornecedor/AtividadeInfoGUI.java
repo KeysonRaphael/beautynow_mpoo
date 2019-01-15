2a92dc1e541e922d157cf55fcf6409d0238a1174
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-01-15 16:53:48 -0300

    gui update info atividade

--- app/src/main/java/kn/beautynow/gui/clienteefornecedor/AtividadeInfoGUI.java
+++ app/src/main/java/kn/beautynow/gui/clienteefornecedor/AtividadeInfoGUI.java
@@ -7,12 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import kn.beautynow.R;
 import kn.beautynow.dominio.clienteefornecedor.Agenda;
 import kn.beautynow.dominio.clienteefornecedor.Atividade;
 import kn.beautynow.dominio.controller.Session;
+import kn.beautynow.negocio.usuario.UsuarioNegocio;
 
 public class AtividadeInfoGUI extends Fragment {
     private OnFragmentInteractionListener mListener;
@@ -41,26 +43,44 @@ public class AtividadeInfoGUI extends Fragment {
         TextView hora = INF.findViewById(R.id.inputhoraview);
         hora.setText(atividade.getHora());
         TextView cliente = INF.findViewById(R.id.inputclienteview);
-        cliente.setText(atividade.getCliente());
         TextView fornecedor = INF.findViewById(R.id.inputfornecedorview);
         fornecedor.setText(atividade.getFornecedor());
+        Button confirmarAtendimento = INF.findViewById(R.id.confirmarAtendimento);
+        Button rejeitarAtendimento = INF.findViewById(R.id.rejeitarAtendimento);
         if (Session.getSession(getContext()).getTipoUsuario().equals("Cliente")){
-          cliente.setVisibility(View.INVISIBLE);
-          TextView clientetexto = INF.findViewById(R.id.clienteview);
-          clientetexto.setVisibility(View.INVISIBLE);
+            cliente.setVisibility(View.INVISIBLE);
+            TextView clientetexto = INF.findViewById(R.id.clienteview);
+            clientetexto.setVisibility(View.INVISIBLE);
+            confirmarAtendimento.setVisibility(View.INVISIBLE);
+            rejeitarAtendimento.setVisibility(View.INVISIBLE);
+            fornecedor.setText(new UsuarioNegocio(getContext()).buscarUsarioPorTipo(atividade.getFornecedor(),"Fornecedor").getNome());
         }else{
-          fornecedor.setVisibility(View.INVISIBLE);
+            fornecedor.setVisibility(View.INVISIBLE);
             TextView fornecedortexto = INF.findViewById(R.id.fornecedorview);
             fornecedortexto.setVisibility(View.INVISIBLE);
+            cliente.setText(new UsuarioNegocio(getContext()).buscarUsarioPorTipo(atividade.getCliente(),"Cliente").getNome());
         }
         TextView servico = INF.findViewById(R.id.inputservicoview);
         servico.setText(atividade.getServico());
         TextView valor = INF.findViewById(R.id.inputvalorview);
         valor.setText(atividade.getValor());
         TextView ativo = INF.findViewById(R.id.inputativoview);
-        ativo.setText(atividade.getAtivo());
         TextView finalizado = INF.findViewById(R.id.inputfinalizadoview);
-        finalizado.setText(atividade.getFinalizado());
+        if (atividade.getAtivo().equals("N")){
+            ativo.setText("Não");
+            finalizado.setVisibility(View.INVISIBLE);
+            TextView finalizadotext = INF.findViewById(R.id.finalizadoview);
+            finalizadotext.setVisibility(View.INVISIBLE);
+        }else if(atividade.getAtivo().equals("S")){
+            ativo.setText("Sim");
+        }else{
+            ativo.setText("Não, Atendimento Rejeitado");
+        }
+        if (atividade.getFinalizado().equals("N")){
+            finalizado.setText("Não");
+        }else {
+            finalizado.setText("Sim");
+        }
         return INF;
     }
 
